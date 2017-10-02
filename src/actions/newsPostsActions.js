4daabd61bb930d0948fd0d6fce3c4a1fdc225d43
Sharon Njihia <sharonnjihia@Sharons-MacBook-Pro.local>
2017-10-02 15:16:12 +0300

    news-api(set-up): set up required directories for react-redux project

--- src/actions/newsPostsActions.js
+++ src/actions/newsPostsActions.js
@@ -9,9 +9,9 @@ export function getPostsFailure(error) {
   return { type: types.FAILURE_TO_GET_POSTS, error };
 }
 
-export default function getAllNewsPosts(source) {
+export default function getAllNewsPosts(source, sortBy) {
   return function (dispatch) {
-    return GetNewsPost.getAllNewsPosts(source)
+    return GetNewsPost.getAllNewsPosts(source, sortBy)
         .then((posts) => {
           dispatch(getPostsSuccess(posts));
         })
--- src/api/getNewsPost.js
+++ src/api/getNewsPost.js
@@ -1,8 +1,8 @@
 const apiKey = '213327409d384371851777e7c7f78dfe';
 
 class GetNewsPost {
-  static getAllNewsPosts(source) {
-    return fetch(`https://newsapi.org/v1/articles?source=${source}&sortBy=latest&apiKey=${apiKey}`)
+  static getAllNewsPosts(source, sortBy) {
+    return fetch(`https://newsapi.org/v1/articles?source=${source}&sortBy=${sortBy}&apiKey=${apiKey}`)
         .then(response => response.json())
         .catch(error => error);
   }
